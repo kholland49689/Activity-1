54b1afe9f17f47e4b786c791e9adf44a446fbc71
Kristen Holland <106491974+kholland49689@users.noreply.github.com>
2022-05-29 16:16:06 -0400

    Add files via upload

---
+++ activity1cst150.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CST_150__Activity_1
+{
+    public partial class activity1cst150 : Form
+    {
+        public activity1cst150()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello! Thank you for clicking the button!");
+        }
+    }
+}
